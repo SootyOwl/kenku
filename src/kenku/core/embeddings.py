45f784c21b5944bf1a7ab11c3cfe8bdc36ec34ca
Sooty <7614538+SootyOwl@users.noreply.github.com>
2023-07-03 07:54:25 +0100

    feat: :sparkles: add embeddings saving and loading

--- src/kenku/core/embeddings.py
+++ src/kenku/core/embeddings.py
@@ -5,7 +5,7 @@ This module contains the classes and functions for the embeddings used in Kenku.
 Classes:
     Section(Protocol): A protocol for a section of a document, from markdown.py.
     """
-from typing import List, Optional, Tuple, Protocol
+from typing import Dict, List, Optional, Tuple, Protocol
 import openai
 from openai.embeddings_utils import cosine_similarity, get_embedding
 import tiktoken
@@ -28,22 +28,22 @@ class Section(Protocol):
 
 
 class Embeddings:
-    """A class for generating embeddings for sections of ."""
+    """A class for generating embeddings for sections of a document."""
 
     def __init__(
         self,
         sections: List[Section],
         engine: str = EMBEDDINGS_MODEL,
         parallel: bool = False,
-    ):
+    ) -> None:
         """Initialize an Embeddings object."""
-        self.sections = sections
+        self.sections: List[Section] = sections
         """The sections of the document."""
-        self.engine = engine
+        self.engine: str = engine
         """The engine to use for generating embeddings."""
-        self.parallel = parallel
+        self.parallel: bool = parallel
         """Whether to generate embeddings in parallel."""
-        self.embeddings = self._generate_embeddings()
+        self.embeddings: Optional[Dict[Section, List[float]]] = None
         """The embeddings for each section of the document."""
 
     def _generate_embeddings(self) -> dict[Section, List[float]]:
@@ -68,16 +68,20 @@ class Embeddings:
         else:
             # generate the embeddings
             embeddings = [
-                get_embedding(section.to_markdown())
-                for section in self.sections
+                get_embedding(section.to_markdown()) for section in self.sections
             ]
 
         # return the embeddings as a dictionary of section: embedding pairs
         return dict(zip(self.sections, embeddings))
-    
-    
+
+    def generate_embeddings(self) -> None:
+        """Generate embeddings for each section of the document."""
+        self.embeddings = self._generate_embeddings()
+
     def get_embedding(self, section: Section) -> List[float]:
         """Get the embedding for a section."""
+        if self.embeddings is None:
+            self.generate_embeddings()
         return self.embeddings[section]
 
     def get_similar_sections(
@@ -94,9 +98,46 @@ class Embeddings:
                 other_section,
                 cosine_similarity(embedding, self.get_embedding(other_section)),
             )
-            for other_section in self.sections if other_section != section
+            for other_section in self.sections
+            if other_section != section
         ]
         # sort the similarities by the cosine similarity
         similarities.sort(key=lambda x: x[1], reverse=True)
         # return the n most similar sections
         return similarities[:n]
+
+    def get_similar_sections_by_query(
+        self, query: str, n: int = 5
+    ) -> List[Tuple[Section, float]]:
+        """Get the n most similar sections to a given query."""
+        # get the embedding for the query
+        embedding = get_embedding(query)
+        # get the cosine similarity between the query and all sections
+        similarities = [
+            (section, cosine_similarity(embedding, self.get_embedding(section)))
+            for section in self.sections
+        ]
+        # sort the similarities by the cosine similarity
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        # return the n most similar sections
+        return similarities[:n]
+
+    def save(self, path: str = "embeddings.pickle") -> None:
+        """Save the embeddings to a file. Imports the pickle module."""
+        import pickle
+
+        with open(path, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def load(cls, path: str = "embeddings.pickle") -> "Embeddings":
+        """Load embeddings from a file. Imports the pickle module."""
+        import pickle
+
+        with open(path, "rb") as f:
+            embeddings = pickle.load(f)
+
+        if not isinstance(embeddings, Embeddings):
+            raise TypeError("File does not contain embeddings.")
+
+        return embeddings
