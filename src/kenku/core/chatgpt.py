b95b267c192e745572c20f5be0ff1bc7aa548898
Sooty <7614538+SootyOwl@users.noreply.github.com>
2023-07-03 07:59:18 +0100

    feat: :sparkles: add chatgpt interface
    also includes functiontemplategenerator, used to turn python functions into a JSON object that chatGPT can understand

---
+++ src/kenku/core/chatgpt.py
@@ -0,0 +1,299 @@
+"""ChatGPT module for Kenku."""
+
+import inspect
+import json
+from typing import Any, Callable, Dict, List, Literal, Protocol, Tuple
+
+import openai
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+
+# section protocol
+class Section(Protocol):
+    title: str
+    parent: "Section"
+    content: list
+    children: list
+
+
+# embeddings protocol
+class Embeddings(Protocol):
+    sections: list
+    engine: str
+    parallel: bool
+    embeddings: dict
+
+    def _generate_embeddings(self) -> dict:
+        ...
+
+    def generate_embeddings(self) -> None:
+        ...
+
+    def get_embedding(self, section: Section) -> list:
+        ...
+
+    def get_similar_sections(self, section: Section, n: int = 5) -> list:
+        ...
+
+    def save(self, path: str) -> None:
+        ...
+
+    @classmethod
+    def load(cls, path: str) -> "Embeddings":
+        ...
+
+
+class FunctionTemplateGenerator:
+    """Takes a set of callables and tries to build a ChatGPT template for each one using introspection.
+
+    e.g.:
+    Takes the function `get_current_weather(location: string, unit: 'celsius' | 'fahrenheit')` and generates a template like:
+    [
+        {
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                },
+                "required": ["location"],
+            },
+        }
+    ]
+
+    Note that the parameters object is in JSON Schema format, strictly.
+    Each property in the parameters object is a parameter to the function.
+    Docstrings are used to populate the description field for each parameter.
+
+    The callable must define a short docstring for each variable and a type annotation for each variable.
+    """
+
+    def __init__(self, functions: List[Callable]):
+        """Initialize the FunctionTemplateGenerator."""
+        self.functions = functions
+        """The functions to generate templates for."""
+
+    def generate_templates(self) -> List[Dict[str, Any]]:
+        """Generate templates for each function."""
+        return [self._generate_template(function) for function in self.functions]
+
+    def _generate_template(self, function: Callable) -> Dict[str, Any]:
+        """Generate a template for a function."""
+        return {
+            "name": function.__name__,
+            "description": function.__doc__.splitlines()[0],
+            "parameters": self._generate_parameters(function),
+        }
+
+    def _generate_parameters(self, function: Callable) -> Dict[str, Any]:
+        """Generate a parameters object for a function."""
+        parameters = {
+            "type": "object",
+            "properties": {},
+            "required": [],
+        }
+        for name, parameter in inspect.signature(function).parameters.items():
+            if parameter.default == inspect._empty:
+                parameters["required"].append(name)
+            parameters["properties"][name] = {
+                "type": self._get_type(parameter.annotation),
+                "description": self._get_description(function, parameter),
+            }
+            # add default value if it exists
+            if parameter.default != inspect._empty:
+                parameters["properties"][name]["default"] = parameter.default
+
+            # add enum if it exists
+            if parameter.annotation != inspect._empty and hasattr(
+                parameter.annotation, "__args__"
+            ):
+                parameters["properties"][name]["enum"] = parameter.annotation.__args__
+
+        return parameters
+
+    def _get_type(self, annotation: Any) -> str:
+        """Get the type for a parameter."""
+        if annotation == inspect._empty:
+            return "string"
+        if annotation == int:
+            return "integer"
+        if annotation == float:
+            return "number"
+        if annotation == bool:
+            return "boolean"
+        if annotation == list:
+            return "array"
+        if annotation == dict:
+            return "object"
+        return "string"
+
+    def _get_description(self, function: Callable, parameter: inspect.Parameter) -> str:
+        """Get the description for a parameter from the docstring of the function.
+
+        The description is the first line of the docstring that starts with the parameter name.
+
+        e.g.:
+        ```
+        def get_current_weather(location: str, unit: str = "celsius") -> str:
+            \"""Get the current weather in a given location.
+
+            location: The city and state, e.g. San Francisco, CA
+            unit: The unit to return the temperature in, e.g. celsius or fahrenheit
+            \"""
+            ...
+        ```
+        """
+        if docstring := function.__doc__:
+            for line in docstring.splitlines():
+                if line.strip().startswith(parameter.name):
+                    return line.strip().split(":")[1].strip()
+
+
+class KenkuGPTEngine:
+    """Kenku GPT Engine.
+
+    Kenku is a tool for answering questions and generating new information for a D&D campaign.
+
+    Attributes:
+        model (str): The model to use for generating responses. Defaults to "gpt-3.5-turbo".
+        system_messages (List[Dict[str, str]]): The system messages to use for generating responses.
+        messages (List[Dict[str, str]]): The initial messages to use for generating responses. Defaults to None.
+        functions (List[Callable]): The functions to use for generating responses. Defaults to None. Must return a string.
+        function_call ("auto"|"none"|dict["name", str]): The function call to use for generating responses. Defaults to "auto" for automatic detection.
+
+    Methods:
+        get_response: Get a response from the engine.
+    """
+
+    def __init__(
+        self,
+        model: str = "gpt-3.5-turbo",
+        messages: List[Dict[str, str]] = None,
+        functions: List[Callable] = None,
+        function_call: Literal["auto", "none"] | Dict[Literal["name"], str] = "auto",
+    ):
+        """Initialize the engine."""
+        self.model = model
+        """The model to use for generating responses."""
+
+        self.system_messages = [
+            {"role": "system", "content": "You are a chatbot named Kenku. Your goal is to answer questions and generate new information for a D&D campaign."},
+            {"role": "system", "content": "You may request information from the DM's notes by using the provided commands."},
+        ]
+        """The system messages are used to instruct the ChatGPT engine in what it should do."""
+
+        self.messages = messages or []
+        """The initial messages to use for generating responses."""
+
+        self.functions = functions or []
+        """The functions to use for generating responses."""
+
+        self.function_call = function_call
+        """The function call to use for generating responses. 
+        Defaults to "auto" for automatic detection."""
+
+        self._templates = FunctionTemplateGenerator(self.functions).generate_templates()
+        """The templates to use for generating responses."""
+
+        self._functions: dict[str, Callable] = {
+            function.__name__: function for function in self.functions
+        }
+
+    def get_response(self, message: str) -> str:
+        """Main method for getting a response from the engine. Handles all logic for generating a response,
+        including parsing function calls from the ChatGPT response, and providing function responses
+        to the ChatGPT engine for further processing.
+
+        Args:
+            message (str): The message to get a response for.
+
+        Returns:
+            str: The final response to the message, after parsing function calls and calling the appropriate function.
+        """
+        if not message:
+            return ""
+
+        # build the prompt of all messages to be sent to the model
+        self._add_user_message(message)
+
+        # generate the response from the model
+        response = self._generate_response()
+
+        # add the response to the conversation
+        self.messages.append(response)
+
+        # parse the response for function calls
+        i = 0
+        while (function_calls := self._parse_function_calls(response))[0] is not None:
+            # get the function responses
+            function_responses = self._get_function_response(function_calls)
+            # add the function responses to the conversation
+            self.messages.append(function_responses)
+            # generate a new response from the model
+            response = self._generate_response()
+            # add the response to the conversation
+            self.messages.append(response)
+        # return the final response
+        return response
+
+    def _add_user_message(self, message: str) -> None:
+        """Add a user message to the conversation."""
+        self.messages.append({"content": message, "role": "user"})
+
+    def _add_system_message(self, message: str) -> None:
+        """Add a system message to the conversation."""
+        self.system_messages.append({"content": message, "role": "system"})
+
+    @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(5))
+    def _generate_response(self) -> Dict[str, str]:
+        """Generate a response from the model."""
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.system_messages.copy() + self.messages.copy(),
+            functions=self._templates,
+            function_call=self.function_call,
+        )
+
+        return response.choices[0]["message"]
+
+    def _parse_function_calls(self, response: dict) -> Tuple[str, List[Dict[str, Any]]]:
+        """Check a response to see if GPT wanted to call a function."""
+        if response.get("function_call"):
+            # function call arguments are returned as a json string, so we need to parse them
+            response_args = json.loads(
+                response["function_call"]["arguments"]
+            )
+            return (
+                response["function_call"]["name"],
+                response_args,
+            )
+        return None, None
+
+    def _get_function_response(
+        self, function_calls: Tuple[str, List[Dict[str, Any]]]
+    ) -> Dict[str, str]:
+        """Get the response from the functions."""
+        function_name, arguments = function_calls
+        function = self._functions[function_name]
+        response = function(**arguments)
+
+        return dict(
+            role="function",
+            name=function_name,
+            content=response,
+        )
+
+    def save_conversation(self, path: str) -> None:
+        """Save the conversation to a file."""
+        with open(path, "w") as f:
+            json.dump(self.messages, f, indent=4)
+
+    def load_conversation(self, path: str) -> None:
+        """Load a conversation from a file."""
+        with open(path, "r") as f:
+            self.messages = json.load(f)
---
+++ tests/test_openai.py
@@ -0,0 +1,317 @@
+"""Tests for the chatGPT component of the app.
+
+Ensures the interface to the OpenAI API is working as expected.
+
+Uses the openai Python package to interface with the OpenAI ChatCompletions API.
+Also tests that chatGPT can call the functions to glean more context from the 
+embeddings of the document.
+"""
+
+import os
+
+import openai
+import pytest
+
+from kenku.core.chatgpt import FunctionTemplateGenerator, KenkuGPTEngine
+
+from .conftest import skip_openai
+
+
+# test the function template generator
+def test_function_template_generator_simple():
+    # define a test function with a docstring and a function body
+    def test_function():
+        """This is a test function."""
+        print("Hello, world!")
+
+    # create a function template generator
+    ftg = FunctionTemplateGenerator([test_function])
+
+    out = ftg.generate_templates()
+    assert len(out) == 1
+    assert out[0]["name"] == "test_function"
+    assert out[0]["description"] == "This is a test function."
+    assert out[0]["parameters"]["type"] == "object"
+    assert out[0]["parameters"]["properties"] == {}
+    assert out[0]["parameters"]["required"] == []
+
+
+def test_function_template_generator_with_parameters():
+    # define a test function with a docstring and a function body
+    def test_function(a: int, b: str = "hello"):
+        """This is a test function.
+
+        Args:
+            a: This is the first parameter.
+            b: This is the second parameter.
+        """
+        print("Hello, world!")
+
+    # create a function template generator
+    ftg = FunctionTemplateGenerator([test_function])
+
+    out = ftg.generate_templates()
+    assert len(out) == 1
+    assert out[0]["name"] == "test_function"
+    assert out[0]["description"] == "This is a test function."
+    assert out[0]["parameters"]["type"] == "object"
+    assert out[0]["parameters"]["properties"] == {
+        "a": {"type": "integer", "description": "This is the first parameter."},
+        "b": {
+            "type": "string",
+            "description": "This is the second parameter.",
+            "default": "hello",
+        },
+    }
+    assert out[0]["parameters"]["required"] == ["a"]
+
+
+class Response:
+    """Mock response object for the OpenAI API.
+
+    Has a choices attribute. This is a list of dictionaries. Each dictionary
+    has a "message" key, which contains a dictionary with a "role" key and
+    a "content" key.
+
+    choices = [
+        {
+            "message": {
+                "role": "assistant",
+                "content": None,
+                "function_call": {
+                    "name": None,
+                    "arguments": {},
+                },
+            },
+        }
+    ]
+    """
+
+    def __init__(self, choices):
+        self.choices = choices
+
+
+# test the chatGPT engine
+@skip_openai
+class TestKenkuGPTEngine:
+    # add a fixture to create the engine
+    @pytest.fixture
+    def engine(self):
+        return KenkuGPTEngine()
+
+    @pytest.fixture
+    def response(self):
+        return Response(
+            [
+                {
+                    "message": {"role": "assistant", "content": "Hello, world!"},
+                }
+            ]
+        )
+
+    @pytest.fixture
+    def response_with_function_call(self):
+        return Response(
+            [
+                {
+                    "message": {
+                        "role": "assistant",
+                        "content": None,
+                        "function_call": {
+                            "name": "get_current_weather",
+                            "arguments": '{\n  "location": "Glasgow, Scotland",\n  "format": "celsius"\n}',
+                        },
+                    },
+                }
+            ]
+        )
+
+    def test_init(self, engine):
+        # check the attributes
+        assert engine.function_call == "auto"
+        assert engine.system_messages == []
+        assert engine._templates == []
+        assert engine.messages == []
+        assert engine.model == "gpt-3.5-turbo"
+
+    # test the private methods
+    def test__add_user_message(self, engine):
+        # add a user message
+        engine._add_user_message("Hello, world!")
+
+        # check the messages
+        assert engine.messages == [
+            {
+                "role": "user",
+                "content": "Hello, world!",
+            }
+        ]
+
+    def test__add_system_message(self, engine):
+        # add a system message
+        engine._add_system_message("Hello, world!")
+
+        # check the messages
+        assert engine.system_messages == [
+            {
+                "role": "system",
+                "content": "Hello, world!",
+            }
+        ]
+
+    def test__parse_function_calls(self, engine, response_with_function_call):
+        # parse the function calls
+        f_name, f_args = engine._parse_function_calls(
+            response_with_function_call.choices[0]["message"]
+        )
+
+        # check the function name and arguments
+        assert f_name == "get_current_weather"
+        assert f_args == {
+            "location": "Glasgow, Scotland",
+            "format": "celsius",
+        }
+
+    def test__parse_function_calls_no_function_call(self, engine):
+        # parse the function calls
+        f_name, f_args = engine._parse_function_calls(
+            {
+                "role": "assistant",
+                "content": "Hello, world!",
+            }
+        )
+
+        # check the function name and arguments
+        assert f_name is None
+        assert f_args is None
+
+    @pytest.fixture
+    def mock_openai(self, mocker):
+        # mock the openai.ChatCompletion.create method
+        return mocker.patch("kenku.core.chatgpt.openai.ChatCompletion.create")
+
+    def test__generate_response(self, engine, mock_openai, response):
+        # mock the response from the OpenAI API
+        mock_openai.return_value = response
+        # get a response
+        response = engine._generate_response()
+
+        # check the response
+        assert response["content"] == "Hello, world!"
+        assert response["role"] == "assistant"
+        assert mock_openai.call_count == 1
+
+    def test__generate_response_with_function_call(
+        self, engine, mock_openai, response_with_function_call
+    ):
+        # mock the response from the OpenAI API
+        mock_openai.return_value = response_with_function_call
+        # get a response
+        response = engine._generate_response()
+
+        # check the response
+        assert response["content"] is None
+        assert response["role"] == "assistant"
+        assert mock_openai.call_count == 1
+
+    # test the public methods
+    def test_get_response_no_function_call(self, engine, mock_openai, response, mocker):
+        # mock the response from the OpenAI API
+        mock_openai.return_value = response
+        # mock the KenkuGPTEngine methods so we can check they are called
+        mocker.patch.object(engine, "_add_user_message", return_value=None)
+        mocker.patch.object(engine, "_parse_function_calls", return_value=(None, None))
+
+        # get a response
+        response = engine.get_response("Hello, world!")
+
+        # check the response
+        assert response["content"] == "Hello, world!"
+        assert response["role"] == "assistant"
+        assert mock_openai.call_count == 1
+        assert engine._add_user_message.call_count == 1
+        assert engine._parse_function_calls.call_count == 1
+
+    def test_get_response_with_function_call(
+        self, engine, mock_openai, response_with_function_call, mocker
+    ):
+        # mock the response from the OpenAI API
+        mock_openai.return_value = response_with_function_call
+        # mock the KenkuGPTEngine methods so we can check they are called
+        mocker.patch.object(engine, "_add_user_message", return_value=None)
+        # mock the _parse_function_calls method to return a function call first, then (None, None)
+        parse_function_calls_mock = mocker.patch.object(engine, "_parse_function_calls")
+        parse_function_calls_mock.side_effect = [
+            (
+                "get_current_weather",
+                {
+                    "location": "Glasgow, Scotland",
+                    "format": "celsius",
+                },
+            ),
+            (None, None),
+        ]
+        mocker.patch.object(
+            engine,
+            "_get_function_response",
+            return_value=dict(
+                role="function",
+                name="get_current_weather",
+                content="It's raining",
+            ),
+        )
+
+        # get a response
+        response = engine.get_response("Hello, world!")
+
+        # check the response
+        assert response["content"] is None
+        assert response["role"] == "assistant"
+        assert mock_openai.call_count == 2
+        assert engine._add_user_message.call_count == 1
+        assert parse_function_calls_mock.call_count == 2
+        assert engine._get_function_response.call_count == 1
+
+
+@skip_openai
+class TestKenkuGPTEngineConversation:
+    """Test having a conversation with the chatGPT engine."""
+
+    @pytest.fixture
+    def engine(self, mocker):
+        """Set up the engine."""
+
+        # define a function to get context about a topic
+        def get_context(query: str) -> dict:
+            """Get more context about the topic specified in the query.
+
+            Args:
+                query (str): The topic to get more information about.
+            """
+            return "This is the context about the topic"
+
+        # create the engine
+        engine = KenkuGPTEngine(
+            function_call="auto",
+            model="gpt-3.5-turbo",
+            functions=[get_context],
+        )
+
+        # return the engine
+        return engine
+
+    def test_chatgpt_calls_function_when_asked(self, engine, mocker):
+        """Test that the chatGPT engine calls the function when asked."""
+        mocker.patch.object(
+            engine,
+            "_get_function_response",
+            return_value={
+                "role": "function",
+                "name": "get_context",
+                "content": "Port Damali is a city on the Menagerie Coast.",
+            },
+        )
+        # get a response
+        response = engine.get_response("Tell me about Port Damali")
+        assert response
+        assert engine._get_function_response.call_count == 1
